a051acc37c871ff767e4380409e3de4bfa489fd2
s_tajima <tajima1989@gmail.com>
2012-05-08 10:02:29 +0900

    add _init.sh

---
+++ _init.sh
@@ -0,0 +1,4 @@
+echo -e "\n#####\n## written by MY-RCFILES\n#####\nsource ~/MY-RCFILES/bashrc\n" >> ~/.bashrc
+echo -e "\n#####\n## written by MY-RCFILES\n#####\nsource ~/MY-RCFILES/vimrc\n" >> ~/.vimrc
+echo -e "\n#####\n## written by MY-RCFILES\n#####\nsource $HOME/MY-RCFILES/screenrc\n" >> ~/.screenrc
+
