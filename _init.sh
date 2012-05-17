9bc1a47a0398562be65ad88f1a90ff1234883d15
s_tajima <tajima1989@gmail.com>
2012-05-17 16:57:38 +0900

    edit _init.sh

--- _init.sh
+++ _init.sh
@@ -1,4 +1,40 @@
-echo -e "\n#####\n## written by MY-RCFILES\n#####\nsource ~/MY-RCFILES/bashrc\n" >> ~/.bashrc
-echo -e "\n#####\n## written by MY-RCFILES\n#####\nsource ~/MY-RCFILES/vimrc\n" >> ~/.vimrc
-echo -e "\n#####\n## written by MY-RCFILES\n#####\nsource $HOME/MY-RCFILES/screenrc\n" >> ~/.screenrc
+#!/bin/sh
 
+sig=`cat <<EOF
+###
+# added by ~/MY-RCFILES/_init.sh
+###
+EOF`
+
+rcfile="$HOME/.bashrc"
+source="source ~/MY-RCFILES/bashrc"
+
+if grep "${source}" ${rcfile} > /dev/null; then
+	echo "${source} is found"
+else
+	echo "${source} is not found"
+	echo -e "\n${sig}\n${source}\n" >> ${rcfile}
+	echo "add ${source} to ${rcfile}"
+fi
+
+rcfile="$HOME/.vimrc"
+source="source ~/MY-RCFILES/vimrc"
+
+if grep "${source}" ${rcfile} > /dev/null; then
+	echo "${source} is found"
+else
+	echo "${source} is not found"
+	echo -e "\n${sig}\n${source}\n" >> ${rcfile}
+	echo "add ${source} to ${rcfile}"
+fi
+
+rcfile="$HOME/.screenrc"
+source="source $HOME/MY-RCFILES/screenrc"
+
+if grep "${source}" ${rcfile} > /dev/null; then
+	echo "${source} is found"
+else
+	echo "${source} is not found"
+	echo -e "\n${sig}\n${source}\n" >> ${rcfile}
+	echo "add ${source} to ${rcfile}"
+fi
