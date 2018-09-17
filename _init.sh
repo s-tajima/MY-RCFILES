c3f3efd5beb4b12fc38391b0e98c9df29b5100b9
Satoshi Tajima <tajima1989@gmail.com>
2018-09-17 23:41:14 +0900

    fix

--- _init.sh
+++ _init.sh
@@ -12,21 +12,13 @@ sig_vimrc=`cat <<EOF
 """
 EOF`
 
-RED="\e[31m"
-GRN="\e[32m"
-END="\e[00m"
-
 ##
 # bashrc
 ##
 rcfile="$HOME/.bashrc"
 source="source ~/MY-RCFILES/bashrc"
-if grep "${source}" ${rcfile} > /dev/null; then
-	echo -e "${source} is ${RED}found${END}"
-else
-	echo -e "${source} is ${GRN}not found${END}"
-	echo -e "\n${sig}\n${source}\n" >> ${rcfile}
-	echo -e "add ${source} to ${rcfile}"
+if ! grep "${source}" ${rcfile} > /dev/null; then
+	echo "\n${sig}\n${source}\n" >> ${rcfile}
 fi
 
 ##
@@ -34,12 +26,8 @@ fi
 ##
 rcfile="$HOME/.vimrc"
 source="source ~/MY-RCFILES/vimrc"
-if grep "${source}" ${rcfile} > /dev/null; then
-	echo -e "${source} is ${RED}found${END}"
-else
-	echo -e "${source} is ${GRN}not found${END}"
-	echo -e "\n${sig_vimrc}\n${source}\n" >> ${rcfile}
-	echo -e "add ${source} to ${rcfile}"
+if ! grep "${source}" ${rcfile} > /dev/null; then
+	echo "\n${sig_vimrc}\n${source}\n" >> ${rcfile}
 fi
 
 ##
@@ -47,12 +35,8 @@ fi
 ##
 rcfile="$HOME/.screenrc"
 source="source $HOME/MY-RCFILES/screenrc"
-if grep "${source}" ${rcfile} > /dev/null; then
-	echo -e "${source} is ${RED}found${END}"
-else
-	echo -e "${source} is ${GRN}not found${END}"
-	echo -e "\n${sig}\n${source}\n" >> ${rcfile}
-	echo -e "add ${source} to ${rcfile}"
+if ! grep "${source}" ${rcfile} > /dev/null; then
+	echo "\n${sig}\n${source}\n" >> ${rcfile}
 fi
 
 ##
@@ -60,12 +44,8 @@ fi
 ##
 #rcfile="$HOME/.inputrc"
 #source="source $HOME/MY-RCFILES/inputrc"
-#if grep "${source}" ${rcfile} > /dev/null; then
-#	echo -e "${source} is ${RED}found${END}"
-#else
-#	echo -e "${source} is ${GRN}not found${END}"
-#	echo -e "\n${sig}\n${source}\n" >> ${rcfile}
-#	echo -e "add ${source} to ${rcfile}"
+#if ! grep "${source}" ${rcfile} > /dev/null; then
+#	echo "\n${sig}\n${source}\n" >> ${rcfile}
 #fi
 
 ##
@@ -73,10 +53,6 @@ fi
 ##
 rcfile="$HOME/.vim"
 source="$HOME/MY-RCFILES/vim"
-if [ -L ${rcfile} ]; then
-	echo -e "${rcfile} is ${RED}found${END}"
-else
-	echo -e "${rcfile} is ${GRN}not found${END}"
-	ln -s "${source}" "${rcfile}" > /dev/null
-	echo -e "made synbolic link ${source} to ${rcfile}"
+if [ ! -L ${rcfile} ]; then
+	ln -sf "${source}" "${rcfile}" > /dev/null
 fi
